03b841eb76e187c3571f95acb41037d72695698c
Kristine Jensen <kristine.b.jensen@gmail.com>
2017-02-25 15:23:57 -0600

    working on object builder

--- server/public/client.js
+++ server/public/client.js
@@ -1,5 +1,10 @@
 var operationsArray = ['Add', 'Subtract', 'Multiply', 'Divide']; // array to dynamically create operations buttons
-var operationsComponents = {}; // empty object to send bundled object to server-side logic
+
+var operationsComponents = { // empty object to send values and operation type to server logic
+  firstValue: 0,
+  secondValue: 0,
+  operationType: ''
+};
 
 $('document').ready(function(){
   console.log('jquery is working');
@@ -9,6 +14,15 @@ $('document').ready(function(){
   function appendButtons(array){ // function to dynamically append operations buttons to DOM
     for (var i = 0; i < array.length; i++) {
       $('#operationsButtons').append('<button class="button" id="' + array[i] + '">' + array[i] + '</button>');
-    }
-  }
-});
+    } // end of for loop in appendButtons function - do not remove
+  } // end of appendButtons functions - do not remove
+
+$('.button').on('click', function(){
+  
+}); // end of event listener - do not remove
+
+
+
+
+
+}); // end of document ready function - do not remove

26b332be754df8996b088b64a1030f6d59c04752
Kristine Jensen <kristine.b.jensen@gmail.com>
2017-02-25 12:58:44 -0600

    Dynamically appending operations buttons to DOM

--- server/public/client.js
+++ server/public/client.js
@@ -1,5 +1,14 @@
+var operationsArray = ['Add', 'Subtract', 'Multiply', 'Divide']; // array to dynamically create operations buttons
+var operationsComponents = {}; // empty object to send bundled object to server-side logic
+
 $('document').ready(function(){
   console.log('jquery is working');
 
+  appendButtons(operationsArray); // calling appendButtons function
 
+  function appendButtons(array){ // function to dynamically append operations buttons to DOM
+    for (var i = 0; i < array.length; i++) {
+      $('#operationsButtons').append('<button class="button" id="' + array[i] + '">' + array[i] + '</button>');
+    }
+  }
 });

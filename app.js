4aa01d562e7f2fd8735865e6b55a7b06b576e5fe
Kristine Jensen <kristine.b.jensen@gmail.com>
2017-02-26 11:24:46 -0600

    Still working on server response functionality

--- app.js
+++ app.js
@@ -12,14 +12,14 @@ app.use(express.static('server/public'));
 app.post('/subtract', function(req, res){
   var newObject = req.body; // assigning new object to variable to access properties
   var subtractTotal = subtract(newObject.x, newObject.y); // calling subtract function
-  var finalSubtractTotal = subtractTotal.toString();
-  res.send(finalSubtractTotal);
 
   function subtract(x, y){ // function to complete subtraction
     var total = x - y;
     return total;
   }
-  res.sendStatus(200);
+
+  res.send(subtractTotal.toString());
+
 }); // end of app.post function
 
 
